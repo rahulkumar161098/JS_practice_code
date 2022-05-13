2129b0186c1a22381f3cb788804b7cc1b9cab436
rahulkumar161098 <rahulkumar161098@gmail.com>
2022-05-13 20:51:59 +0530

    initial commit

---
+++ proType.js
@@ -0,0 +1,15 @@
+// prototype
+// function Students(first, last, age) {
+//     this.firstName = first
+//     this.lastName = last
+//     this.age = age
+// }
+// student1 = new Students('Rahul', 'Kumar', 22)
+// student2 = new Students('abhi', 'Kumar', 21)
+
+// student1.class = 3
+
+// // prototype syntex
+// Students.prototype.class = 3
+
+// console.log(student2);
